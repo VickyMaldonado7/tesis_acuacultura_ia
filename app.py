60784d6dd7f15c455df1a662dc25214c032161fd
VickyMaldonado7 <mariamaldonado@uees.edu.ec>
2026-05-14 22:35:47 +0000

    Actualizacion

--- app.py
+++ app.py
@@ -331,35 +331,36 @@ if st.button("Predecir riesgo"):
         color = "#2ecc71"
         icono = "🟢"
 
-    st.markdown(f"""
-    <div style="
-        background: linear-gradient(135deg, {color}, #111827);
-        padding: 28px;
-        border-radius: 18px;
-        color: white;
-        text-align: center;
-        box-shadow: 0 8px 25px rgba(0,0,0,0.25);
-        margin-top: 25px;
-        margin-bottom: 28px;
-    ">
-
+    st.markdown(
+        f"""
         <div style="
-            font-size: 46px;
-            font-weight: 800;
-            margin-bottom: 8px;
+            background: linear-gradient(135deg, {color}, #111827);
+            padding: 28px;
+            border-radius: 18px;
+            color: white;
+            text-align: center;
+            box-shadow: 0 8px 25px rgba(0,0,0,0.25);
+            margin-top: 25px;
+            margin-bottom: 28px;
         ">
-            {icono} RIESGO {pred}
-        </div>
 
-        <div style="
-            font-size: 22px;
-            font-weight: 500;
-        ">
-            Confianza del modelo: {confianza:.1f}%
-        </div>
+            <div style="
+                font-size: 46px;
+                font-weight: 800;
+                margin-bottom: 8px;
+            ">
+                {icono} RIESGO {pred}
+            </div>
 
-    </div>
-    """, unsafe_allow_html=True)
+            <div style="
+                font-size: 22px;
+                font-weight: 500;
+            ">
+                Confianza del modelo: {confianza:.1f}%
+            </div>
+
+        </div>
+        """, unsafe_allow_html=True)
 
     st.subheader("Recomendación general")
     st.write(recomendacion_general)
