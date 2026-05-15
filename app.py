987a6fb5795e06d65dc11428ee7e8bc1e83f62de
VickyMaldonado7 <mariamaldonado@uees.edu.ec>
2026-05-15 02:16:48 +0000

    Actualizacion de ID

--- app.py
+++ app.py
@@ -91,14 +91,15 @@ with col_inputs:
     col1, col2 = st.columns(2)
 
     with col1:
+        id_piscina = st.text_input("ID Piscina", placeholder="Ej: CAM-12A")
         tan = st.number_input("TAN", format="%.3f")
         nh3 = st.number_input("NH3", format="%.3f")
         no2 = st.number_input("NO2", format="%.3f")
         no3 = st.number_input("NO3", format="%.3f")
         po4 = st.number_input("PO4", format="%.3f")
-        sulfuro = st.number_input("Sulfuro", format="%.3f")
-        
+
     with col2:
+        sulfuro = st.number_input("Sulfuro", format="%.3f")
         alk = st.number_input("Alcalinidad", format="%.2f")
         ph = st.number_input("pH", format="%.2f")
         temp = st.number_input("Temperatura", format="%.2f")
@@ -110,6 +111,7 @@ with col_img:
 
 st.markdown("")
 
+# Generar recomendaciones para cada parametro
 def generar_recomendaciones(tan, nh3, no2, no3, po4, sulfuro, alk, ph, temp, salinidad, r_np, pred):
     alertas = []
 
@@ -267,6 +269,7 @@ def guardar_caso(data, pred, confianza, recomendacion_general, alertas_principal
 
     registro = data.copy()
     registro["ID_CASO"] = numero_caso
+    registro["ID_PISCINA"] = data["ID_PISCINA"].iloc[0]
     registro["PREDICCION_NRS"] = pred
     registro["CONFIANZA_MODELO"] = round(confianza, 2)
     registro["RECOMENDACION_GENERAL"] = recomendacion_general
@@ -325,10 +328,16 @@ if st.button("Predecir riesgo"):
         st.info("💡 Estos valores son poco probables en condiciones reales o pueden indicar error de medición.")
         st.stop()
 
+    if id_piscina.strip() == "":
+        st.warning("Debe ingresar un ID de piscina.")
+        st.stop()
+
     data = pd.DataFrame(
         [[tan, nh3, no2, no3, po4, sulfuro, alk, ph, temp, salinidad, r_np]],
         columns=["TAN", "NH3T", "NO2", "NO3", "PO4", "SULFURO", "ALK", "PH", "TEMP", "SALINIDAD", "R_NP"]
     )
+
+    data["ID_PISCINA"] = id_piscina
 
     pred = modelo.predict(data)[0]
 
